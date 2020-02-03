258ff0e277180f7de11056ef94d3ed996100c10d
aLi <aaljamid@emich.edu>
2020-02-03 05:50:31 -0500

    done with all functions v1. Need to enhacne the code more

--- js/script.js
+++ js/script.js
@@ -1,40 +1,139 @@
 $(document).ready(function() {
+  // Test the link with the Index.html
   console.log("live");
 
-  //   let boxes = $("th");
-
-  //   function r1t1() {
-  //     //   $("img").append(" src=img/x.png");
-  //     $(".r1t1").append(
-  //       " <img src='img/x.png' alt='Genral Assembly' width='10px' />"
-  //     );
-  //     console.log("works");
-  //   }
-
-  //   $(".r1t1").click(r1t1);
-
-  //DOM manipulation code
-
+  // declare the variables
   let boxes = $(".box");
   let count = 0;
+
+  // adding click event
   function clickEvent() {
     if (count % 2 === 0) {
-      console.log("even");
       this.classList += " cross";
     } else {
-      console.log("odd");
       this.classList += " circle";
     }
-
+    count++;
     this.removeEventListener("mouseleave", leaveEvent);
     this.removeEventListener("mouseover", hoverEvent);
+    this.removeEventListener("click", clickEvent);
 
-    for (let i = 0; i < boxes.length; i++) {
-      if (boxes[i].classList.value.includes("cross")) {
-        console.log(count);
+    //wins conditions for all the rows (3)
+
+    //1
+    if (
+      boxes[0].classList.value == boxes[1].classList.value &&
+      boxes[1].classList.value == boxes[2].classList.value
+    ) {
+      if (boxes[0].classList.value.includes("cross")) {
+        console.log("cross won 1");
+        $(".winner").html(" <h2>cross won</h2>");
+        stopGame();
+      }
+      if (boxes[0].classList.value.includes("circle")) {
+        console.log("circle won");
+        stopGame();
       }
     }
-    count++;
+
+    // 2
+    else if (
+      boxes[3].classList.value == boxes[4].classList.value &&
+      boxes[3].classList.value == boxes[5].classList.value
+    ) {
+      if (boxes[3].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[3].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    //3
+    else if (
+      boxes[6].classList.value == boxes[7].classList.value &&
+      boxes[7].classList.value == boxes[8].classList.value
+    ) {
+      if (boxes[6].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[6].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    //wins conditions for all the column (3)
+
+    // 1
+    else if (
+      boxes[0].classList.value == boxes[3].classList.value &&
+      boxes[3].classList.value == boxes[6].classList.value
+    ) {
+      if (boxes[0].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[0].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+    // 2
+    else if (
+      boxes[1].classList.value == boxes[4].classList.value &&
+      boxes[4].classList.value == boxes[7].classList.value
+    ) {
+      if (boxes[1].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[1].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    //3
+    else if (
+      boxes[2].classList.value == boxes[5].classList.value &&
+      boxes[5].classList.value == boxes[8].classList.value
+    ) {
+      if (boxes[2].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[2].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    //wins conditions for all the corners (2)
+
+    //1
+    else if (
+      boxes[0].classList.value == boxes[4].classList.value &&
+      boxes[4].classList.value == boxes[8].classList.value
+    ) {
+      if (boxes[0].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[0].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    //2
+    else if (
+      boxes[2].classList.value == boxes[4].classList.value &&
+      boxes[4].classList.value == boxes[6].classList.value
+    ) {
+      if (boxes[2].classList.value.includes("cross")) {
+        console.log("cross won");
+      }
+      if (boxes[2].classList.value.includes("circle")) {
+        console.log("circle won");
+      }
+    }
+
+    /// No one wins
+    else {
+      console.log("DRAW");
+    }
   }
 
   function hoverEvent() {
@@ -50,4 +149,16 @@ $(document).ready(function() {
     boxes[i].addEventListener("mouseover", hoverEvent);
     boxes[i].addEventListener("mouseleave", leaveEvent);
   }
+
+  const stopGame = function() {
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].removeEventListener("mouseleave", leaveEvent);
+      boxes[i].removeEventListener("mouseover", hoverEvent);
+      boxes[i].removeEventListener("click", clickEvent);
+    }
+  };
+
+  $(".playAgain").click(function() {
+    location.reload(true);
+  });
 });
